ccd07244f7979b8a375b95d89169df07cfbd060b
zeph180 <zephrichards1@gmail.com>
2024-11-20 17:49:51 +0300

    Add sdk to android

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,22 @@
 package io.ionic.starter;
 
+import com.nexgo.smartpos.sdk.APIProxy;
+import com.nexgo.smartpos.sdk.DeviceEngine;
+import com.nexgo.smartpos.sdk.module.beeper.Beeper;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get the global object for device operations
+        DeviceEngine deviceEngine = APIProxy.getDeviceEngine(this);
+
+        // Example: Get the beeper object and ring for 500 milliseconds
+        Beeper beeper = deviceEngine.getBeeper();
+        beeper.beep(500);
+    }
+}
+
